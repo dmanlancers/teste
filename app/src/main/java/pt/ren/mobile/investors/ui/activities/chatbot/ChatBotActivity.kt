6413e79f9cfbbd64cb10894056def0a853b7c701
REN\EX001281 <crodrigues@ren.pt>
2020-02-21 17:39:24 +0000

    Chatbot layouts V2

--- app/src/main/java/pt/ren/mobile/investors/ui/activities/chatbot/ChatBotActivity.kt
+++ app/src/main/java/pt/ren/mobile/investors/ui/activities/chatbot/ChatBotActivity.kt
@@ -1,13 +1,11 @@
 package pt.ren.mobile.investors.ui.activities.chatbot
 
 import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
 import android.view.ViewGroup.MarginLayoutParams
 import android.view.inputmethod.InputMethodManager
-import android.widget.ScrollView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_chatbot.*
@@ -19,6 +17,7 @@ import pt.ren.mobile.investors.utils.shared_preferences.SharedPreference
 
 class ChatBotActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chatbot)
@@ -28,7 +27,7 @@ class ChatBotActivity : AppCompatActivity() {
         user_logo.visibility = View.INVISIBLE
         chatbot1_next_floating_button.visibility = View.INVISIBLE
        val marginBlock01 = block01_container?.layoutParams as MarginLayoutParams
-        val marginBlock02 = block02_container?.layoutParams as MarginLayoutParams
+       // val marginBlock02 = block02_container?.layoutParams as MarginLayoutParams
          val SplashDisplayLenght =1000  //splash screen will be shown for 3 seconds
         chatbot1_next_floating_button.setImageResource(R.drawable.ic_right_arow)
 
@@ -52,7 +51,7 @@ class ChatBotActivity : AppCompatActivity() {
          Handler().postDelayed({
              marginBlock01.topMargin= 900
              chatbot_scrollview.post(Runnable { chatbot_scrollview.scrollTo(0, txt_name.top) })
-             marginBlock02.topMargin = 800
+            // marginBlock02.topMargin = 800
          },SplashDisplayLenght.toLong())
         /* chatbot_scrollview.post(Runnable { chatbot_scrollview.fullScroll(ScrollView.FOCUS_DOWN) })
          chatbot_scrollview.post(Runnable { chatbot_scrollview.scrollTo(0, txt_block01.bottom) })*/
